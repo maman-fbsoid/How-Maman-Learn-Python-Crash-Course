0d233b93555371d81595cff638ebcec7dcfe0029
Maman <swafreport89@gmail.com>
2026-06-26 11:15:22 -0400

    Create contoh.py

---
+++ 02-Variable/contoh.py
@@ -0,0 +1,26 @@
+## Variabel
+
+pesan = "pagi manusia hitam!"
+print(pesan)
+
+## Strings
+
+barang = "Meja"
+teman = "Budi"
+sepeda = "Astrea"
+print(f"\nTemanku yaitu {teman} membawakanku sebuah {barang} ke rumahku dengan sepeda {sepeda}")
+
+## contoh string ga efisien tapi kadang kepake
+
+tanaman = "Sawit"
+makanan = "Seblak"
+print("Tanamanku adalah" + tanaman + "dan suka makan" + makanan ".")
+
+## besar/kecil
+
+name = "budi"
+print(name.title())
+
+bocah = "Yudi Basikal"
+print(bocah.upper())
+print(bocah.lower())
