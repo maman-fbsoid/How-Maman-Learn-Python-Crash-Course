a672bd70dd223fdfa9e4b9b3ba0589e5c34d3844
Maman <swafreport89@gmail.com>
2026-06-26 22:14:40 -0400

    Create contoh.py

---
+++ 03-List/contoh.py
@@ -0,0 +1,8 @@
+## List
+teman = ['budi', 'epan', 'topek', 'maman'] ## <- Ini list
+print(teman)
+
+## .append()
+teman = ['budi', 'epan', 'topek', 'maman']
+teman.append('bambang') ## <- ini metode manambahkan
+print(teman)
